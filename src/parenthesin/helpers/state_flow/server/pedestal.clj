d3c460a5e79469ca4d48ae9d621d68b641fad726
Rafael Delboni <rafadelboni@gmail.com>
2025-03-16 15:36:52 -0300

    feat: improves flow request fn to work with x-www-form-urlencoded

--- src/parenthesin/helpers/state_flow/server/pedestal.clj
+++ src/parenthesin/helpers/state_flow/server/pedestal.clj
@@ -2,12 +2,17 @@
   (:require [cheshire.core :as json]
             [clojure.string :as string]
             [io.pedestal.test :as pt]
+            [ring.util.codec :as codec]
             [state-flow.api :as state-flow.api]
             [state-flow.core :as state-flow :refer [flow]]))
 
 (defn- do-request [service-fn verb route body headers]
   (let [headers-with-default (merge {"Content-Type" "application/json"} headers)
-        encoded-body (json/encode body)]
+        content-type (get headers-with-default "Content-Type")
+        encoded-body (case content-type
+                       "application/json" (json/encode body)
+                       "application/x-www-form-urlencoded" (codec/form-encode body)
+                       body)]
     (pt/response-for service-fn verb route :headers headers-with-default :body encoded-body)))
 
 (defn- parsed-response
@@ -17,6 +22,9 @@
     request))
 
 (defn request!
+  "Flow that make http request using `:io.pedestal.http/service-fn`, not a fully
+  featured http client, currently only supports content type `application/json`
+  and `application/x-www-form-urlencoded`."
   [{:keys [method uri body headers]}]
   (flow "makes http request"
     [service-fn (state-flow.api/get-state (comp :io.pedestal.http/service-fn :webserver :webserver))]
